5b5b9704c878e8e30d54b7e8e17bc435489388c0
nojus <me@nojus.dev>
2024-02-17 00:21:14 +0200

    Auto to backboard

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -10,15 +10,30 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(768);
 
+        double backboardX = 72 - 24 - 0.0; // todo adjust live
+        double backboardY = 1 * (-24 * 1.5);
+        double ROBOT_LENGTH = 15.9;
+        double ROBOT_WIDTH = 16.9;
+
+        boolean isLeft = false;
+
+        double xDelta = (-12 + ROBOT_WIDTH / 2) + 4;
+
+        double yDelta = (24 * 2 - ROBOT_LENGTH / 2) - 8; // Position of prop relative to start point
+
+        int allianceCoef = 1;
+
+
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 13.77)
                 .setDimensions(16.92, 15.74)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(12.20, -63.86, Math.toRadians(90.00)))
-                                .lineTo(new Vector2d(12.20, -72 + 24 * 2 - 7 - 1))
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -72 + ROBOT_LENGTH / 2.0, Math.toRadians(90)))
+                                .splineToLinearHeading(new Pose2d(-xDelta + 24,
+                                        -72 + ROBOT_LENGTH / 2.0 + allianceCoef * yDelta,
+                                        Math.toRadians(180)), Math.toRadians(180))
                                 .build()
-
                 );
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VLRAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VLRAuto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -47,40 +48,74 @@ public class VLRAuto extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
         TrajectoryActionBuilder navBuilder = drive.actionBuilder(startPose);
 
-        double xDelta, yDelta;
+        // Left or right
+        boolean isLeft = propPosition == Camera.PropPos.LEFT;
+
+        double xDelta = (-12 + cfg.ROBOT_WIDTH / 2) + 4;
+        xDelta = isLeft ? xDelta : -xDelta;
+
+        double yDelta = (24 * 2 - cfg.ROBOT_LENGTH / 2) - 8; // Position of prop relative to start point
 
         int allianceCoef = isRed ? 1 : -1; // 1 for red, -1 for blue
 
-        if (propPosition == Camera.PropPos.CENTER) {
-            yDelta = -24 - cfg.ROBOT_LENGTH / 2.0 - 6;
-            navBuilder = navBuilder.lineToY(allianceCoef * yDelta);
+        if ((isRed && isNearBackboard && propPosition == Camera.PropPos.RIGHT) ||
+                (!isRed && !isNearBackboard && propPosition == Camera.PropPos.LEFT)) {
+            navBuilder = navBuilder.splineToLinearHeading(new Pose2d(xDelta + 24 + 0.0, // todo adjust live
+                    startPose.position.y + allianceCoef * yDelta,
+                    Math.toRadians(180)), Math.toRadians(180));
         } else {
-            // Left or right
-            boolean isLeft = propPosition == Camera.PropPos.LEFT;
+            if (propPosition == Camera.PropPos.CENTER) {
+                yDelta = -24 - cfg.ROBOT_LENGTH / 2.0 - 6;
+                navBuilder = navBuilder.lineToY(allianceCoef * yDelta);
+            } else {
+                telemetry.addData("MAIN", "xDelta: " + xDelta);
+                telemetry.update();
 
-            yDelta = (24 * 2 - cfg.ROBOT_LENGTH / 2) - 8; // Position of prop relative to start point
-            xDelta = (-12 + cfg.ROBOT_WIDTH / 2) + 4;
-            xDelta = isLeft ? xDelta : -xDelta;
-            telemetry.addData("MAIN", "xDelta: " + xDelta);
-            telemetry.update();
+                double angle = 0; // Angle the robot should be facing to hit the prop
+                if (isLeft && isRed) angle = Math.PI;
+                if (!isLeft && !isRed) angle = Math.PI;
 
-            double angle = 0; // Angle the robot should be facing to hit the prop
-            if (isLeft && isRed) angle = Math.toRadians(180);
-            if (!isLeft && !isRed) angle = Math.toRadians(180);
+                // Determine prop position on field plane
+                Pose2d placePos = new Pose2d(startPose.position.x + xDelta,
+                        startPose.position.y + allianceCoef * yDelta, angle);
 
-            // Determine prop position on field plane
-            Pose2d placePos = new Pose2d(startPose.position.x + xDelta,
-                    startPose.position.y + allianceCoef * yDelta, angle);
-
-            // Move up to not hit the pillar on the left / right while turning
-            navBuilder = navBuilder.lineToY(startPose.position.y + allianceCoef * (yDelta / 1.5))
-                    // and go to the prop
-                    .splineToLinearHeading(placePos, angle);
+                // Move up to not hit the pillar on the left / right while turning
+                navBuilder = navBuilder.lineToY(startPose.position.y + allianceCoef * (yDelta / 1.5))
+                        // and go to the prop
+                        .splineToLinearHeading(placePos, angle);
+            }
         }
 
         navBuilder = navBuilder.waitSeconds(0.1).afterTime(0.2, claw::ToggleClawLeft)
-                .afterTime(0.4, () -> claw.clawRotator.setPosition(0.2))
-                .waitSeconds(0.9);
+                .afterTime(0.4, () -> claw.clawRotator.setPosition(0.2)).waitSeconds(0.3);
+
+        // Move to backboard
+        double backboardX = 72 - 24 - 8.0; // todo adjust live
+        double backboardY = allianceCoef * (-24 * 1.5);
+
+        if (!isNearBackboard) {
+            if (propPosition != Camera.PropPos.CENTER) {
+                // Advance forward
+                navBuilder = navBuilder.lineToY(allianceCoef * (-24 + 0.0)) // todo adjust live
+                        .splineToLinearHeading(new Pose2d(-36, -12 * allianceCoef, Math.toRadians(0)), Math.toRadians(0));
+            } else {
+                // Move to the left / right
+                navBuilder = navBuilder.lineToX(-72 + 24 / 2 + 6.0) // todo adjust live
+                        .splineToLinearHeading(new Pose2d(-36, -12 * allianceCoef, Math.toRadians(0)), Math.toRadians(0));
+            }
+            navBuilder = navBuilder.lineToX(72 - 24 * 2)
+                    .splineToLinearHeading(new Pose2d(backboardX, backboardY, Math.toRadians(180)), Math.toRadians(180));
+        } else {
+            // can just proceed to backboard
+            navBuilder = navBuilder.lineToX(backboardX);
+        }
+
+        double apriltagDeltaY = 0;
+        if (propPosition == Camera.PropPos.LEFT) apriltagDeltaY = -4.5;
+        if (propPosition == Camera.PropPos.RIGHT) apriltagDeltaY = 4.5;
+
+        navBuilder.lineToY(backboardY + apriltagDeltaY);
+        // todo arm extension bullshit
 
         //////////////////////////////////////////////////////////
         Actions.runBlocking(navBuilder.build());
