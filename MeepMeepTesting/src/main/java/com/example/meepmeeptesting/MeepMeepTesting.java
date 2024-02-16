5f2d012d89aa44f808fb6ace4df7cae647870e73
Vilte <sapvilte@gmail.com>
2024-02-16 18:39:53 +0200

    Finish auto backboard

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -6,21 +6,22 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+
 public class MeepMeepTesting {
+
     public static void main(String[] args) {
+
         MeepMeep meepMeep = new MeepMeep(768);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 13.77)
                 .setDimensions(16.92, 15.74)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-34, -34 + 12, Math.toRadians(90.00)))
-                                .splineTo(new Vector2d(24, -12), Math.toRadians(0))
-                                .splineToLinearHeading(new Pose2d(12*4, -34, Math.toRadians(180)), Math.toRadians(0))
-                                .build()
 
-                );
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(-36, -37.95, Math.toRadians(90)))
+                        .build());
+
 
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VLRAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VLRAuto.java
@@ -52,26 +52,47 @@ public class VLRAuto extends LinearOpMode {
 
         int allianceCoef = isRed ? 1 : -1; // 1 for red, -1 for blue
 
+        boolean isLeft = propPosition == Camera.PropPos.LEFT;
+
+        double angle = 0; // Angle the robot should be facing to hit the prop
+        if (isLeft && isRed) angle = Math.toRadians(180);
+        if (!isLeft && !isRed) angle = Math.toRadians(180);
+
         if (propPosition == Camera.PropPos.CENTER) {
-            yDelta = -24 - cfg.ROBOT_LENGTH / 2.0 - 6;
+            // robot length = 15.9
+            yDelta = -24 - cfg.ROBOT_LENGTH / 2.0 - 6; // -37.95
             navBuilder = navBuilder.lineToY(allianceCoef * yDelta);
+            //           BACKBOARD
+            //  (12, 37.95)     (12, -37.95)
+            //  (-36, 37.95)    (-36, -37.95)
+
         } else {
             // Left or right
-            boolean isLeft = propPosition == Camera.PropPos.LEFT;
+            //boolean isLeft = propPosition == Camera.PropPos.LEFT;
 
-            yDelta = (24 * 2 - cfg.ROBOT_LENGTH / 2) - 8; // Position of prop relative to start point
-            xDelta = (-12 + cfg.ROBOT_WIDTH / 2) + 4;
+            yDelta = (24 * 2 - cfg.ROBOT_LENGTH / 2) - 8; // Position of prop relative to start point (32.05)
+            xDelta = (-12 + cfg.ROBOT_WIDTH / 2) + 4; // 0.45
             xDelta = isLeft ? xDelta : -xDelta;
             telemetry.addData("MAIN", "xDelta: " + xDelta);
             telemetry.update();
 
-            double angle = 0; // Angle the robot should be facing to hit the prop
-            if (isLeft && isRed) angle = Math.toRadians(180);
-            if (!isLeft && !isRed) angle = Math.toRadians(180);
+            //double angle = 0; // Angle the robot should be facing to hit the prop
+            //if (isLeft && isRed) angle = Math.toRadians(180);
+            //if (!isLeft && !isRed) angle = Math.toRadians(180);
 
             // Determine prop position on field plane
             Pose2d placePos = new Pose2d(startPose.position.x + xDelta,
                     startPose.position.y + allianceCoef * yDelta, angle);
+            //          BACKBOARD
+            // !isLeft              !isLeft
+            // (11.55, 32, (180))   (11.55, -32, (0))
+            // isLeft               isLeft
+            // (12,45, 32, (0))     (12.45, -32, (180))
+            //
+            // !isLeft              !isLeft
+            // (-36.45, 32, (180))  (-36.45, -32 (0))
+            // isLeft               isLeft
+            // (-35.55, 32, (0))    (-35.55, -32 (180))
 
             // Move up to not hit the pillar on the left / right while turning
             navBuilder = navBuilder.lineToY(startPose.position.y + allianceCoef * (yDelta / 1.5))
@@ -86,27 +107,31 @@ public class VLRAuto extends LinearOpMode {
 
 
             if (isNearBackboard)
+                //WORKS CORRECTLY
                 if (propPosition != Camera.PropPos.CENTER) {
+                    navBuilder = navBuilder.lineToY(startPose.position.y/1.2)
+                            .splineToLinearHeading(
+                                    new Pose2d((startPose.position.x*4), startPose.position.y + allianceCoef*yDelta, Math.toRadians(180)), Math.toRadians(0));
                 }
                 else{
-                    navBuilder = navBuilder.lineToX(startPose.position.x + cfg.ROBOT_LENGTH)
+                    navBuilder = navBuilder.lineToY( yDelta*1.1)
+                            .lineToX(startPose.position.x * 2)
                             .splineToLinearHeading(
-                                    new Pose2d((startPose.position.x + cfg.ROBOT_LENGTH),
-                                            (startPose.position.y + allianceCoef * (yDelta / 1.5)),
-                                            Math.toRadians(180)),  Math.toRadians(0));
+                                new Pose2d(startPose.position.x * 4, startPose.position.y - allianceCoef * yDelta, Math.toRadians(180)), Math.toRadians(0));
                 }
             else {
                 if (propPosition != Camera.PropPos.CENTER) {
-                    navBuilder = navBuilder.splineTo(new Vector2d(24, -12), Math.toRadians(0))
-                            .splineToLinearHeading(new Pose2d(12*4, -34, Math.toRadians(180)), Math.toRadians(0));
+                    navBuilder = navBuilder.lineToY(startPose.position.y + allianceCoef * (yDelta * 1.3))
+                            .splineToLinearHeading(new Pose2d(-12, allianceCoef * (-yDelta / 2.5), Math.toRadians(angle)), Math.toRadians(0)); // Math.toRadians should be the same as the variable angle
+
+
+                } else {
+                    navBuilder = navBuilder.lineToX(startPose.position.x*1.625)
+                            .splineToLinearHeading(new Pose2d(-12*4, (startPose.position.y + allianceCoef * yDelta) / 3, Math.toRadians(180)), Math.toRadians(0))
+                    ;
                 }
-                else {
-                    navBuilder = navBuilder.lineToX(startPose.position.x + cfg.ROBOT_LENGTH)
-                            .splineToLinearHeading(
-                                    new Pose2d((startPose.position.x + cfg.ROBOT_LENGTH),
-                                            (startPose.position.y + allianceCoef * (yDelta / 1.5)),
-                                            Math.toRadians(180)), Math.toRadians(0));
-                }
+                navBuilder = navBuilder.lineToX(24)
+                        .splineToLinearHeading(new Pose2d(startPose.position.x*(-1.3), startPose.position.y - allianceCoef*yDelta, Math.toRadians(180)), Math.toRadians(0));
             }
         //////////////////////////////////////////////////////////
         Actions.runBlocking(navBuilder.build());
